8008fa80f5baa11bfa02df523dbb728a9c7f4649
kdev307 <kumardev30072002@gmail.com>
2023-12-11 20:48:08 +0530

    Major UI Upgradations

---
+++ javascript/modalView.js
@@ -0,0 +1,19 @@
+// let modalShowBtn = document.getElementById("overlay-btn");
+// modalShowBtn.addEventListener("click", function () {
+//     document.getElementById("header").style.opacity = "0.1";
+//     document.getElementById("home").style.opacity = "0.1";
+//     document.getElementById("about").style.opacity = "0.1";
+//     document.getElementById("programs").style.opacity = "0.1";
+//     document.getElementById("facilities").style.opacity = "0.1";
+//     document.getElementById("testimonials").style.opacity = "0.1";
+//     document.getElementById("cta").style.opacity = "0.1";
+//     document.getElementById("footer").style.opacity = "0.1";
+// });
+
+function showOverlay() {
+    document.getElementById("overlay-section").style.display = "block";
+}
+
+function hideOverlay() {
+    document.getElementById("overlay-section").style.display = "none";
+}
