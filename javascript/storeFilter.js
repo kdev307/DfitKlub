94a082039222e64f19c4674e0dc36ce4d6547ba1
kdev307 <kumardev30072002@gmail.com>
2023-12-12 21:55:17 +0530

    Store Page Layout Updated

---
+++ javascript/storeFilter.js
@@ -0,0 +1,19 @@
+const filterContainer = document.querySelector(".product-filter");
+const productItems = document.querySelectorAll(".product-item");
+
+filterContainer.addEventListener("click", (event) => {
+    if (event.target.classList.contains("filter-item")) {
+        filterContainer.querySelector(".active").classList.remove("active");
+        event.target.classList.add("active");
+        const filterValue = event.target.getAttribute("data-filter");
+        productItems.forEach((item) => {
+            if (item.classList.contains(filterValue) || filterValue === "all") {
+                item.classList.remove("hide");
+                item.classList.add("show");
+            } else {
+                item.classList.remove("show");
+                item.classList.add("hide");
+            }
+        });
+    }
+});
